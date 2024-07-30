0c48f30d0d1bfb3ffdd7cb8df56c8857ded72e1e
한덕용 <dukyong99@naver.com>
2024-07-30 17:47:32 +0900

    if, loop, function study

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ChunjaeJavascriptStudy
+
+if문 switch로 바꾸기
+switch문 if로 바꾸기
+
+<!-- 시험점수
+
+91 ~ 100 : A
+81 ~ 90 : B
+71 ~ 80 : C
+61 ~ 70 : D
+~ 60 : F -->
---
+++ test.js
@@ -0,0 +1,76 @@
+// // var num1 = 10;
+// // var num2 = 0.1;
+
+// // console.log(typeof num1);
+// // console.log(typeof num2);
+
+// // console.log("num1 + num2 : " + num1 + num2);
+// // console.log(num1 + num2);
+
+// // var num3 = 12;
+// // var num4 = "qwer";
+// // var num5 = "100";
+// // var num6 = "21qw3236e";
+// // var num7 = "qw3236e";
+
+// // console.log(num3 + parseInt(num6));
+// // console.log(num3 + parseInt(num7));
+// // console.log(parseInt("abc"));
+
+// // var bool1 = true;
+// // var bool2 = false;
+// // var bool3 = 1;
+// // var bool4 = 0;
+// // // console.log(bool1);
+// // // console.log(bool2);
+// // // console.log(bool3);
+// // console.log(bool1 && bool3);
+// // console.log(bool1 && bool4);
+// // console.log(bool2 && bool3);
+// // console.log(bool2 && bool4);
+
+// // var bool5 = "";
+// // if (bool1 && bool3) {
+// //   bool5 = bool1 && bool3;
+// // }
+// // console.log(bool5);
+
+// var num1 = 10;
+// var num2 = 100;
+// if (num1 < num2) {
+//   console.log("num2가 큼");
+// } else {
+//   console.log("num1이 큼");
+// }
+
+// var arrScores = [
+//   80, 90, 100, 80, 90, 100, 80, 90, 100, 80, 90, 100, 80, 90, 100, 80, 90, 100,
+// ];
+// // console.log(arrScores);
+// console.log(arrScores[1][0]);
+
+// var score = prompt("시험 점수를 입력하세요. : ");
+// if (score > 90) {
+//   console.log("A");
+// } else if (score > 90) {
+//   console.log("B");
+// } else if (score < 90 && score >= 80) {
+//   console.log("C");
+// } else if (score < 80 && score >= 70) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// var i = 0;
+// var sum = 0;
+// for (i = 1; i < 10; i++) {
+//   sum += 1;
+//   document.write("i" + i + "sum" + sum);
+// }
+
+// var arrSting = ["a", "b", "c"];
+// var arr2 = new array("a", "b", "c");
+
+let a = function () {};
+let b = () => {};
